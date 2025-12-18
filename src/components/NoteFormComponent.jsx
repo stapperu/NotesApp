3dd0e594eb91c0e5211691e0e5626cff718cb748
stapperu <k.kubiak94@gmail.com>
2025-12-18 20:17:26 +0100

    changed state from separate states to one object state with title,priority,category,description all in one

--- src/components/NoteFormComponent.jsx
+++ src/components/NoteFormComponent.jsx
@@ -1,38 +1,92 @@
-import { useState,useEffectEvent } from "react";
-
+import { useState, useEffectEvent } from "react";
+// alternative with each state separate, requires changing onChange to "onChange={(e)=>{setPriority(e.target.value)}}" and value to whatever the state is instead of "formData.x"
 const NoteFormComponent = () => {
-    const [title,setTitle]=useState();
-    const [priority,setPriority]=useState('Medium');
-    const [category,setCategory]=useState('Work');
-    const [description,setDescription]=useState('');
+//     const [title,setTitle]=useState();
+//     const [priority,setPriority]=useState('Medium');
+//     const [category,setCategory]=useState('Work');
+//     const [description,setDescription]=useState(''); }
 
-    return ( 
-        <>
-     
-        <form className="mb-6 p-4 pb-8 bg-gray-100 shadow-lg shadow-gray-100" action="">
-          <div className="mb-4">
-             <label htmlFor="name" className=" text-center text-md font-bold block">Title</label>
-        <input className="bg-amber-50 border-b block mx-auto my-2 w-full max-w-80" type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}}/>
-       </div>
-          <div className="mb-4"><label htmlFor="name" className="mx-3 text-md font-bold block ">Priority</label>
-                <select className="bg-amber-50 border-b- mx-auto my-2 w-full" type="text" value={priority} onChange={(e)=>{setPriority(e.target.value)}}>
-                <option value="High">High</option>
-                <option value="Medium">Medium</option>
-                <option value="Low">Low</option>
-                </select> </div>
-          <div className="mb-4"><label htmlFor="name" className="mx-3 text-md font-bold block">Category</label>
-                <select className="bg-amber-50 border-b mx-auto my-2 w-full" type="text" value={category} onChange={(e)=>{setCategory(e.target.value)}}>
-                <option value="Work">Work</option>
-                <option value="Personal">Personal</option>
-                <option value="Ideas">Ideas</option>
-                 </select> </div>
-          <div className="mb-4"><label htmlFor="name" className=" text-md font-bold block">Description</label>
-                <textarea className="bg-amber-50 border-b mx-auto my-2 w-full" type="text" rows="5" value={description} onChange={(e)=>{setDescription(e.target.value)}}/> </div>
-    <button className="block mx-auto p-6 text-xl rounded-md bg-amber-300 hover:bg-amber-400 text-white">Add Note</button>
+const [formData, setFormData] = useState({
+	title: "",
+	priority: "",
+	category: "",
+	description: "",
+});
+const handleChange = (e) => {
+	setFormData({
+		...formData,
+		[e.target.name]: e.target.value,
+	});
+};
 
-        </form>
-        </>
-     );
-}
- 
+return (
+	<>
+		<form
+			className="mb-6 p-4 pb-8 bg-gray-100 shadow-lg shadow-gray-100"
+			action=""
+		>
+			<div className="mb-4">
+				<label htmlFor="name" className=" text-center text-md font-bold block">
+					Title
+				</label>
+				<input
+					className="bg-amber-50 border-b block mx-auto my-2 w-full max-w-80"
+					name="title"
+					type="text"
+					value={formData.title}
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="mb-4">
+				<label htmlFor="name" className="mx-3 text-md font-bold block ">
+					Priority
+				</label>
+				<select
+					className="bg-amber-50 border-b- mx-auto my-2 w-full"
+					type="text"
+					name="priority"
+					value={formData.priority}
+					onChange={handleChange}
+				>
+					<option value="High">High</option>
+					<option value="Medium">Medium</option>
+					<option value="Low">Low</option>
+				</select>{" "}
+			</div>
+			<div className="mb-4">
+				<label htmlFor="name" className="mx-3 text-md font-bold block">
+					Category
+				</label>
+				<select
+					className="bg-amber-50 border-b mx-auto my-2 w-full"
+					type="text"
+					name="category"
+					value={formData.category}
+					onChange={handleChange}
+				>
+					<option value="Work">Work</option>
+					<option value="Personal">Personal</option>
+					<option value="Ideas">Ideas</option>
+				</select>{" "}
+			</div>
+			<div className="mb-4">
+				<label htmlFor="name" className=" text-md font-bold block">
+					Description
+				</label>
+				<textarea
+					className="bg-amber-50 border-b mx-auto my-2 w-full"
+					type="text"
+					rows="5"
+					name="description"
+					value={formData.description}
+					onChange={handleChange}
+				/>{" "}
+			</div>
+			<button className="block mx-auto p-6 text-xl rounded-md bg-amber-300 hover:bg-amber-400 text-white">
+				Add Note
+			</button>
+		</form>
+	</>
+)};
+
 export default NoteFormComponent;
